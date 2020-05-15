fc3413abab4b81e50fca3535ed2dc3da0fc0412a
pidoxy <56538561+pidoxy@users.noreply.github.com>
2020-05-15 16:06:22 +0100

    I fixed the line-through for completed tasks, but it works only for the first task only

--- javascript/app.js
+++ javascript/app.js
@@ -5,40 +5,54 @@
 
 let list = document.getElementById('list');
 
-
+let id = "id" + Math.random().toString(16).slice(2);
 
 function createTodo() {
 
-    let id = "id" + Math.random().toString(16).slice(2);
+    
 
     let todo = document.createElement('li');//creates an <li> tag in the html document
     todo.setAttribute('id', id);
-    
-    //checkbox for todo
+
+
     let done = document.createElement('input');
     done.setAttribute('type', 'checkbox');
-    // done.setAttribute('checked', '');
-    
+    done.setAttribute('id', 'check');
+    done.setAttribute('class', 'checkIt')
+    done.setAttribute('onclick', 'completed();');
     todo.appendChild(done);
+
 
     let inputValue = document.getElementById("input").value;
     console.log(inputValue); //debug
     let newtodo = document.createTextNode(inputValue);
     todo.appendChild(newtodo);
 
-    if(inputValue === ''){
+    if (inputValue === '') {
         alert('Add todo');
     } else {
         list.appendChild(todo);
     }
+    document.getElementById('input').value = ""
 
 }
+//checkbox for todo
 
-//  completed = () => {
-//      if(checked) {
 
-//      }
-//  }
+function completed() {
+    let checkBox = document.getElementById('check');
+    let check = document.getElementById(id);
+    
+    if (checkBox.checked === true) {
+        console.log('sgsg');
+        check.style.textDecoration = 'line-through';
+      }  else if ( checkBox.checked === false) {
+        console.log( 'line-through');
+        check.style.textDecoration = 'none';
+    
+     }
+ 
+}
 
 
 
