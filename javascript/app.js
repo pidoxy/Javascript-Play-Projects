776175e2751dd5255be7a9d06e81e3d7981b8512
pidoxy <56538561+pidoxy@users.noreply.github.com>
2020-05-19 18:32:24 +0100

    update

--- javascript/app.js
+++ javascript/app.js
@@ -9,17 +9,18 @@ let id = "id" + Math.random().toString(16).slice(2);
 
 function createTodo() {
 
-    
+
 
     let todo = document.createElement('li');//creates an <li> tag in the html document
     todo.setAttribute('id', id);
+    todo.setAttribute('class', id);
 
-
+    //checkbox element
     let done = document.createElement('input');
     done.setAttribute('type', 'checkbox');
-    done.setAttribute('id', 'check');
-    done.setAttribute('class', 'checkIt')
     done.setAttribute('onclick', 'completed();');
+    done.setAttribute('id', 'check');
+
     todo.appendChild(done);
 
 
@@ -40,19 +41,30 @@ function createTodo() {
 
 
 function completed() {
+    
     let checkBox = document.getElementById('check');
-    let check = document.getElementById(id);
+    let check = document.getElementsByClassName(id);
     
-    if (checkBox.checked === true) {
-        console.log('sgsg');
-        check.style.textDecoration = 'line-through';
-      }  else if ( checkBox.checked === false) {
-        console.log( 'line-through');
-        check.style.textDecoration = 'none';
-    
-     }
- 
+
+    if (checkBox.checked) {
+        for (let i = 0; i < check.length; i++) {
+
+            check[i].style.textDecoration = 'line-through';
+            check[i].style.backgroundColor = '#ff006a';
+            check[i].style.textDecorationColor = 'black';
+            check[i].style.color = 'white';
+        }
+
+    }  else {
+        for (let i = 0; i < check.length; i++) {
+
+            check[i].style.textDecoration = 'none';
+            check[i].style.backgroundColor = 'white';
+            check[i].style.color = 'black';
+        }
+
+
+    }
+
+
 }
-
-
-
